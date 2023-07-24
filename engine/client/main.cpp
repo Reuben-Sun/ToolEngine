0d2d1f92bd2af70e1a1663f23613dd67d785f08d
Reuben-PC <2824016820@qq.com>
2023-07-25 00:51:59 +0800

    创建窗口

--- engine/client/main.cpp
+++ engine/client/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-#include "test.h"
-#include "imgui.h"
+#include "GLFW/glfw3.h"
+
+static void onErrorCallback(int error_code, const char* description)
+{
+    std::cout << "Error" << std::endl;
+}
 
 int main(){
-    
-    std::cout << add(1, 2) << std::endl;
+    const int SAMPLE_WIDTH = 800;
+    const int SAMPLE_HEIGHT = 400;
+    char* PROJECT_NAME = "Test";
+    glfwSetErrorCallback(onErrorCallback);
+    if (!glfwInit()) { return 1; }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(SAMPLE_WIDTH, SAMPLE_HEIGHT, PROJECT_NAME, nullptr, nullptr);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
