134a5b4337fe618110ae2c66745bba975a9d8cd7
Reuben-PC <2824016820@qq.com>
2023-07-25 18:10:28 +0800

    tbb

--- engine/client/main.cpp
+++ engine/client/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "client_framework.h"
+#include "tbb/tbb.h"
 
 
 int main()
 {
+    tbb::parallel_invoke(
+        []() {std::cout << "TBB!" << std::endl; },
+        []() {std::cout << "Hello" << std::endl; }
+    );
     ToolEngine::ClientFramework framework;
     framework.onStart();
     return 0;
