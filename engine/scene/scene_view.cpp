b64b3d9709ceaa9e69a78856861a51521ba920cf
Reuben-PC <2824016820@qq.com>
2023-07-25 10:33:35 +0800

    scene

---
+++ engine/scene/scene_view.cpp
@@ -0,0 +1,16 @@
+#include "scene_view.h"
+
+namespace ToolEngine
+{
+    SceneView::SceneView()
+    {
+    }
+
+    SceneView::~SceneView()
+    {
+    }
+    int SceneView::Add()
+    {
+        return 1;
+    }
+}
---
+++ engine/scene/scene_view.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ToolEngine
+{
+    class SceneView
+    {
+    public:
+        SceneView();
+        ~SceneView();
+        int Add();
+    };
+}
