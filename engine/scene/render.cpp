ee843bbe2412a82c548e865eca92731c6691af56
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2023-08-04 14:07:56 +0800

    fix swapchain error

--- engine/scene/render.cpp
+++ engine/scene/render.cpp
@@ -222,6 +222,7 @@ namespace ToolEngine
 		createInfo.imageFormat = surfaceFormat.format;
 		createInfo.imageColorSpace = surfaceFormat.colorSpace;
 		createInfo.imageExtent = extent;
+        createInfo.imageArrayLayers = 1;
 		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 		QueueFamilyIndices indices = getQueueFamilyIndices(physicalDevice);
 		uint32_t queueFamilyIndices[] = { indices.graphicsFamily.value(), indices.presentFamily.value() };
