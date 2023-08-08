7784806164aaaf868967bb9734a08b0d95090381
TECH-ET-孙政 <zheng.sun@boomingtech.com>
2023-08-08 17:45:39 +0800

    window

--- engine/scene/window.h
+++ engine/scene/window.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vulkan/vulkan.h>
+#include "include/rhi/instance.h"
 
 namespace ToolEngine
 {
@@ -22,8 +23,12 @@ namespace ToolEngine
 
 		Window(const Properties &properties);
 		virtual ~Window() = default;
-		virtual VkSurfaceKHR createSurface() = 0;
+		virtual VkSurfaceKHR createSurface(Instance &instance) = 0;
+		virtual bool shouldClose() = 0;
+		virtual void processEvents() = 0;
 
+		const Extent& getExtent() const { return m_properties.extent; }
+		Extent resize(const Extent& extent);
 	protected:
 		Properties m_properties;
 	};
