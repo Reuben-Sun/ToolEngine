47b006435f7ccdd6f9165bcd1d3e099a9b7ae5a3
Reuben-Sun <2824016820@qq.com>
2023-09-16 23:26:34 +0800

    texture create

--- engine/core/include/rhi/texture_image.h
+++ engine/core/include/rhi/texture_image.h
@@ -1,24 +1,31 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
-
+#include <string>
 #include "buffer.h"
 #include "device.h"
 #include "physical_device.h"
 #include "include/file/file_utils.h"
+#include "include/rhi/single_time_command_buffer.h"
 
 namespace ToolEngine
 {
 	class TextureImage : Buffer
 	{
 	public:
-		TextureImage(Device& device, PhysicalDevice& physical_device);
+		TextureImage(Device& device, PhysicalDevice& physical_device, std::string file_path);
 		~TextureImage();
 		VkImage getHandle() const { return m_texture_image; }
 
 		void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties);
+		
+		
 	private:
 		VkImage m_texture_image;
 		VkDeviceMemory m_texture_image_memory;
+		VkFormat m_format = VK_FORMAT_R8G8B8A8_SRGB;
+
+		void transitionImageLayout(VkImageLayout old_layout, VkImageLayout new_layout);
+		void createImageFormBuffer(VkBuffer buffer, uint32_t width, uint32_t height);
 	};
 }
