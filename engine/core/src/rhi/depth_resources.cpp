6e48f658916eedb45c7593ca9ed859f552b30bd4
Reuben-PC <2824016820@qq.com>
2023-09-23 17:45:24 +0800

    depth resource

---
+++ engine/core/src/rhi/depth_resources.cpp
@@ -0,0 +1,12 @@
+#include "include/rhi/depth_resources.h"
+
+namespace ToolEngine
+{
+	DepthResources::DepthResources(Device& device, DepthSupportDetails depth_details, VkExtent2D extent): m_device(device)
+	{
+		
+	}
+	DepthResources::~DepthResources()
+	{
+	}
+}
