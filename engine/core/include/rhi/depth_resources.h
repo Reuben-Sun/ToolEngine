cf9ec43528c0ab931b21a93b0273244f7ca643b9
Reuben-PC <2824016820@qq.com>
2023-09-23 18:14:15 +0800

    split image

--- engine/core/include/rhi/depth_resources.h
+++ engine/core/include/rhi/depth_resources.h
@@ -4,20 +4,23 @@
 #include "include/common/depth_support_details.h"
 #include "image_view.h"	
 #include "device.h"
+#include "physical_device.h"
+#include "image.h"
 
 namespace ToolEngine
 {
 	class DepthResources
 	{
 	public:
-		DepthResources(Device& device, DepthSupportDetails depth_details, VkExtent2D extent);
+		DepthResources(Device& device, PhysicalDevice& physical_device, VkExtent2D extent);
 		~DepthResources();
 
 		ImageView& getImageView() { return *m_depth_image_view; }
 
 	private:
-		VkImage m_depth_image;
+		std::unique_ptr<Image> m_depth_image;
 		std::unique_ptr<ImageView> m_depth_image_view;
 		Device& m_device;
+		PhysicalDevice& m_physical_device;
 	};
 }
--- engine/core/src/rhi/depth_resources.cpp
+++ engine/core/src/rhi/depth_resources.cpp
@@ -2,9 +2,13 @@
 
 namespace ToolEngine
 {
-	DepthResources::DepthResources(Device& device, DepthSupportDetails depth_details, VkExtent2D extent): m_device(device)
+	DepthResources::DepthResources(Device& device, PhysicalDevice& physical_device, VkExtent2D extent)
+		: m_device(device), m_physical_device(physical_device)
 	{
-		
+		DepthSupportDetails depth_support_details = DepthSupportDetails::getDepthSupportDetails(m_physical_device.getHandle());
+		m_depth_image = std::make_unique<Image>(m_device, m_physical_device, extent, depth_support_details.support_format, 
+			VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+
 	}
 	DepthResources::~DepthResources()
 	{
