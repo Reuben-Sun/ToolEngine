061482f4d0691527868069687f3b6c2abec1678a
Reuben-ROG <2824016820@qq.com>
2023-10-09 10:39:23 +0800

    scene

---
+++ engine/scene/scene.cpp
@@ -0,0 +1,19 @@
+#include "scene.h"
+
+namespace ToolEngine
+{
+	Scene::Scene()
+	{
+		// TODO: load model
+	}
+
+	Scene::~Scene()
+	{
+
+	}
+
+	void Scene::tick() 
+	{
+
+	}
+}
---
+++ engine/scene/scene.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+namespace ToolEngine
+{
+	class Scene
+	{
+	public:
+		Scene();
+		~Scene();
+
+		void tick();	
+
+	private:
+		// camera
+		// std::vector<Light> 
+		// std::vector<Model> 
+	};
+}
