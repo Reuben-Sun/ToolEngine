9b1c893a73d9a4830989a32cdfcd78051de2114e
Reuben-Sun <2824016820@qq.com>
2023-10-19 22:43:37 +0800

    global render context

---
+++ engine/core/include/render/global_render_context.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace ToolEngine
+{
+	// TODO: store render context
+	// have many pointer of manager, such as particle, texture, render setting
+	// when I want to create a texture, I just need use context->textureManager->createTexture()
+}
