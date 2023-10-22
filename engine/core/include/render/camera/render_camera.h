681629b99542aaaa971f591cd5b3eb85a64de26d
Reuben-PC <2824016820@qq.com>
2023-10-22 22:46:17 +0800

    render camera

--- engine/core/include/render/camera/render_camera.h
+++ engine/core/include/render/camera/render_camera.h
@@ -7,13 +7,15 @@ namespace ToolEngine
 {
 	struct RenderCamera
 	{
+		bool left_handed;
 		float aspect;
 		float view_size;
 		float near_plane;
 		float far_plane;
 		float fov;
-		glm::mat4x4 projection_matrix;
-		glm::mat4x4 view_matrix;
 		Transform transform;
+
+		glm::mat4x4 getProjectionMatrix();
+		glm::mat4x4 getViewMatrix();
 	};
 }
