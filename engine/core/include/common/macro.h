ac76b75ace60d900ec2b3c635862f584ae49bcf5
Reuben-Sun <2824016820@qq.com>
2023-10-30 01:44:30 +0800

    delete PI
    C++20添加了这个常量

--- engine/core/include/common/macro.h
+++ engine/core/include/common/macro.h
@@ -7,8 +7,6 @@
 
 #define FORCEINLINE __forceinline
 
-#define PI 3.1415926535897932384626433832795f
-
 #define LOG_HELPER(LOG_LEVEL, ...) \
     g_global_context.m_log_manager->log(LOG_LEVEL,"[" __FUNCTION__ "] " +std::format(__VA_ARGS__));
 
