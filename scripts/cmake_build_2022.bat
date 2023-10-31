d8b23e6edaa691828e65b5cb34550c1d48f6120f
Reuben-PC <2824016820@qq.com>
2023-11-01 01:28:25 +0800

    build action

--- scripts/cmake_build_2022.bat
+++ scripts/cmake_build_2022.bat
@@ -1,11 +1,9 @@
 @echo off
 
-set SOURCE_DIR=../engine/
-set BUILD_DIR=../build/
+set SOURCE_DIR=.
+set BUILD_DIR=build
 
-cd ../
-mkdir build
-cd engine
+
 cmake -S %SOURCE_DIR% -B %BUILD_DIR% -G "Visual Studio 17 2022"
 cmake --build %BUILD_DIR%
 
