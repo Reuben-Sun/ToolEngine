695b96ae7e4023faa787e4b2f11eb9ced1ef4ac6
Reuben-PC <2824016820@qq.com>
2023-11-01 01:30:27 +0800

    build action

--- scripts/cmake_build_2022.bat
+++ scripts/cmake_build_2022.bat
@@ -1,10 +1,6 @@
 @echo off
 
-set SOURCE_DIR=.
-set BUILD_DIR=build
-
-
-cmake -S %SOURCE_DIR% -B %BUILD_DIR% -G "Visual Studio 17 2022"
-cmake --build %BUILD_DIR%
+cmake -S .-B build -G "Visual Studio 17 2022"
+cmake --build build
 
 pause
