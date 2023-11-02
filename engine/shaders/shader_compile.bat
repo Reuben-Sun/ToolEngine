526581945426e14e32fb628b1f0f4274175debdf
Reuben-PC <2824016820@qq.com>
2023-11-02 22:25:14 +0800

    shader compiler

--- engine/shaders/shader_compile.bat
+++ engine/shaders/shader_compile.bat
@@ -2,9 +2,8 @@
 
 set SHADER_DIR=src/
 set TARGET_DIR=spv/
-set COMPILER_TOOL=glslc.exe
 
-%COMPILER_TOOL% %SHADER_DIR%shader.vert -o %TARGET_DIR%vert.spv
-%COMPILER_TOOL% %SHADER_DIR%shader.frag -o %TARGET_DIR%frag.spv
+glslangValidator -V -S vert %SHADER_DIR%shader.vert -o %TARGET_DIR%vert.spv
+glslangValidator -V -S frag %SHADER_DIR%shader.frag -o %TARGET_DIR%frag.spv
 
 pause
